a6a1072539979f7e288887414a6e4d1073cf0d80
voyagerdva <mariner-9@yandex.ru>
2020-01-18 15:57:21 +0300

    Bask Poland Notation

---
+++ Lection_13_back_poland_notation.py
@@ -0,0 +1,34 @@
+# ==================================================
+#
+#   Lection 13.
+#
+#   asymptotics: O(N)
+#
+# ==================================================
+
+from module import A_stack
+import ast
+
+def isint(item):
+    try:
+        int(item)
+        return True
+    except ValueError:
+        return False
+
+def backPolandNotation(notation:list):
+     for token in notation:
+          if isint(token):
+               A_stack.push(token)
+          else:
+               y = A_stack.pop()
+               x = A_stack.pop()
+               z = eval(str(x) + str(token) + str(y))
+               A_stack.push(z)
+               
+     return A_stack.pop()
+
+notation = ['2', '7', "+"]
+
+print(backPolandNotation(notation))
+
