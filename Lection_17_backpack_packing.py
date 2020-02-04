3053241eb6098975321f1d65bc864682fb160971
voyagerdva <mariner-9@yandex.ru>
2020-02-04 19:29:41 +0300

    backpack packing

--- Lection_17_backpack_packing.py
+++ Lection_17_backpack_packing.py
@@ -1,23 +1,40 @@
 # ==================================================
 #
-#   Lection 17.
-#
-#   asymptotics: O(N*M)
+#    Lection 17.
+#    Packing backpack!
+#    Укладка дискретного конечного рюкзака.
+#    Каждый предмет обладает массой и стоимостью.
+#    asymptotics: O(N*M)
 #
 # ==================================================
 
-def bag(M, N):
-     F = [[0]*(N+1) for i in range(M+1)]
+"""  """
+def bestTotalCost(M, C, W):
+     F = [[0]*(W+1) for i in range(len(M))]
 
-     for i in range(1, N+1):
-          for k in range(1, M+1):
-               if m[i] <= k:
-                    F[k][i] = max(F[k][i-1], v[i] + F[k-m[i]][i-1])
+     for i in range(1, len(M)):
+          for w in range(1, W + 1):
+               if M[i] <= w:
+                    F[i][w] = max(F[i-1][w], F[i-1][w-M[i]] + C[i])
                else:
-                    F[k][i] = F[k][i-1]
-     return F[M][N]
+                    F[i][w] = F[i-1][w]
+     return F, F[i][w]
 
-print(bag(5, 6))
-     
+def insideBackpack(F, M, W):
+     tmp = W
+     backpack = []
+     for i in range(len(M)-1, 0, -1):
+          if F[i][tmp] != F[i-1][tmp]:
+               backpack.append(i)
+               tmp -= M[i]
+     return backpack
+
+thingMass = [0, 7, 3, 1, 5, 4]
+thingCost = [0, 10, 4, 2, 6, 7]
+backpackMass = 12
+
+bestCost = bestTotalCost(thingMass, thingCost, backpackMass)
+print(bestCost[1])
+print(insideBackpack(bestCost[0], thingMass, backpackMass))     
 
                
