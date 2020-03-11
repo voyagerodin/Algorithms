c4c4fdb575789753f480c10737a0192fe7224fdd
voyagerdva <mariner-9@yandex.ru>
2020-03-11 16:54:35 +0300

    heapGUI

--- Lection_19_heap/modules/heapStructure.py
+++ Lection_19_heap/modules/heapStructure.py
@@ -5,51 +5,63 @@
 #
 # ==================================================
 
-
 class HeapStructure:
     def __init__(self):
-        self.values = []
+        self.val = []
         self.size = 0
 
     def clean(self):
-        self.values = []
+        self.val = []
         self.size = 0
 
     def insert(self, x):
-        self.values.append(x)
+        self.val.append(x)
         self.size += 1
         self.siftUp(self.size-1)
 
     def delete(self, n):
-        self.values[n], self.values[self.size -
-                                    1] = self.values[self.size-1], self.values[n]
+        self.val[n], self.val[self.size-1] = self.val[self.size-1], self.val[n]
+        self.val.pop()
+        self.size -= 1
+        self.siftDown(n)
+
+    def siftDown(self, i):
+        while (2*i + 1) < (self.size - 1) and (2*i + 2) < (self.size - 1) and self.val[i] > self.val[2*i + 1] and self.val[i] > self.val[2*i + 2]:
+
+            if self.val[i] > self.val[2*i + 1]:
+                self.val[i], self.val[2*i + 1] = self.val[2*i + 1], self.val[i]
+                i = 2*i + 1
+            elif self.val[i] > self.val[2*i + 2]:
+                self.val[i], self.val[2*i + 2] = self.val[2*i + 2], self.val[i]
+                i = 2*i + 2
 
     def siftUp(self, i):
-        while i != 0 and self.values[i] < self.values[(i-1) // 2]:
-            self.values[i], self.values[(
-                i-1) // 2] = self.values[(i-1) // 2], self.values[i]
+        while i != 0 and self.val[i] < self.val[(i-1) // 2]:
+            self.val[i], self.val[(i-1) // 2] = self.val[(i-1) // 2], self.val[i]
             i = (i-1) // 2
 
     def insertList(self, L):
         for item in L:
-            self.values.append(item)
+            self.val.append(item)
             self.size += 1
             self.siftUp(self.size-1)
 
     def show(self):
-        print(self.values)
+        print(self.val)
 
     def get(self):
-        return self.values
+        return self.val
 
 
 listIn0 = [15, 8, 11, 3, 0, 9]
-listIn1 = [6, 3, 4, 1, 2, 5]
-listIn3 = [66, 1, 7, 1, 10, 22, 6, 29, 83, 2]
+listIn1 = [6, 3, 4, 7, 2, 5]
+listIn3 = [36, 27, 12, 43, 55, 11, 29, 88, 30, 44, 77, 66]
 
 heap = HeapStructure()
-
-heap.insertList(listIn1)
+print(listIn3)
+heap.insertList(listIn3)
 heap.show()
-heap.delete(0)
+# heap.insert(1)
+heap.delete(6)
+
 heap.show()
